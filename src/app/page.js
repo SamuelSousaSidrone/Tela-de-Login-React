f2d68e333eaf652ceca5baa3515ebf0ea2ee07e4
Samuel de Sousa Sidrone <samuelsousasidrone@gmail.com>
2024-07-29 16:53:04 -0300

    finish

---
+++ src/app/page.js
@@ -0,0 +1,67 @@
+"use client"
+
+  import "./globals.css";
+  import React from "react";
+  import {useState} from "react";
+  //import jpIMG from "./assets/jp.svg";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  return(
+  <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title"> Bem vindo a tela de login </span>
+
+            
+
+            <div className="wrap-input">
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input
+                className={username !== "" ? "has-val input" : "input"}
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Username"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input
+                className={password !== "" ? "has-val input" : "input"}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta? </span>
+              <a className="txt2" href="#">
+                Criar conta
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+  
+}
